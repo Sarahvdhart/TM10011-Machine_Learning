6ab3bd759c232487f4ff20a103e43e4b876ad3a8
Sarahvdhart <S.L.vanderHart@student.tudelft.nl>
2026-02-10 23:31:08 +0100

    avond

--- test git.py
+++ test git.py
@@ -1,1 +1,14 @@
-print("hallo")
+#importeren
+import pandas as pd
+
+#inlezen van de dataset
+df = pd.read_csv("Datasets.csv")
+print(df)
+
+#aantal dataset
+aantal_datasets = df["dataset"].count()
+print(aantal_datasets)
+
+#print names van datasets
+names_dataset = set(df["dataset"])
+print(names_dataset)
